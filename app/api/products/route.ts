065cee7bbc9f4e2640952d24f015f602db0fd6ea
Anurag Dhote <anuragdhote392@gmail.com>
2023-12-26 21:54:34 +0530

    feat - API routes for CRUD operations on Products done

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,26 +2,52 @@ import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request: NextRequest) => {
-    try {
-        const products = await prisma.products.findMany({ take: 10 });
-        return NextResponse.json({ products });
+    try { 
+        const params = request.nextUrl.searchParams;
+        const take = params.get('take') || 10;
+        const skip = params.get('skip') || 0;
+        const products = await prisma.products.findMany({ take: Number(take), skip: Number(skip) });
+        return NextResponse.json({ error: undefined, response: products });
     } catch (error) {
-        return NextResponse.json({ error });
+        return NextResponse.json({ error, response: undefined });
     }
 }
 
 export const POST = async (request: NextRequest) => {
     try {
         const products = await request.json();
-        console.log(products);
         const response = await prisma.products.createMany({
             data: products || [],
             skipDuplicates: true
-        })
-
-        return NextResponse.json({ response });
+        });
+        return NextResponse.json({ error: undefined, response });
     } catch (error) {
         console.log(error);
-        return NextResponse.json({ error });
+        return NextResponse.json({ error, response: undefined });
+    }
+}
+
+export const PUT = async (request: NextRequest) => {
+    try {
+        const productBody = await request.json();
+        const { id } = productBody;
+        const originalProduct = await prisma.products.findUnique({ where: { id } });
+        const updatedProduct = await prisma.products.update({
+            where: { id },
+            data: { ...originalProduct, ...productBody }
+        });
+        return NextResponse.json({ error: undefined, response: updatedProduct });
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message, response: undefined });
+    }
+}
+
+export const DELETE = async (request: NextRequest) => {
+    try {
+        const { id } = await request.json();
+        const deletedProduct = await prisma.products.delete({ where: { id } });
+        return NextResponse.json({ error: undefined, response: deletedProduct });
+    } catch (error) {
+        return NextResponse.json({ error, response: undefined });
     }
 }
