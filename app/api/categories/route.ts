17e676df38fe52c95fc63ad2f966758de4a8bc23
Anurag Dhote <anuragdhote392@gmail.com>
2023-12-27 22:35:15 +0530

    feat - complete API routes handlers for categories

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,6 +1,27 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
+export const GET = async (request: NextRequest) => {
+    try {
+        const params = request.nextUrl.searchParams;
+        const id = params.get("id")
+
+        let response;
+        if (id) {
+            response = await prisma.categories.findUniqueOrThrow({
+                where: {
+                    id: Number(id)
+                }
+            })
+        } else {
+            response = await prisma.categories.findMany({})
+        }
+        return NextResponse.json({error: undefined, response})
+    } catch (error) {
+        return NextResponse.json({error, response: undefined})
+    }
+}
+
 export const POST = async (request: NextRequest) => {
     try {
         const { id, name } = await request.json();
@@ -19,5 +40,64 @@ export const POST = async (request: NextRequest) => {
         return NextResponse.json({ error: undefined, response });
     } catch (error) {
         return NextResponse.json({ error, response: undefined });
+    }
+}
+
+export const PUT = async (request: NextRequest) => {
+    try {
+        const { id, name, updated_name } = await request.json();
+        
+        let response;
+        if (id) {
+            response = await prisma.categories.update({
+                where: {
+                    id
+                },
+                data: {
+                    id,
+                    name: updated_name
+                }
+            })
+        } else if (name) {
+            response = await prisma.categories.update({
+                where: {
+                    name
+                },
+                data: {
+                    name: updated_name
+                }
+            })
+        } else {
+            throw new Error("No id or name provided!")
+        }
+        return NextResponse.json({error: undefined, response})
+    } catch (error) {
+        return NextResponse.json({error, response: undefined})
+    }
+}
+
+export const DELETE = async (request: NextRequest) => {
+    try {
+        const { id, name } = await request.json()
+
+        let response;
+        if (id) {
+            response = await prisma.categories.delete({
+                where: {
+                    id
+                }
+            })
+        } else if (name) {
+            response = await prisma.categories.deleteMany({
+                where: {
+                    name
+                }
+            })
+        } else {
+            throw new Error("No id or name provided!")
+        }
+        return NextResponse.json({error: undefined, response})
+    } catch (error) {
+        return NextResponse.json({error, response: undefined})
     }
 }
