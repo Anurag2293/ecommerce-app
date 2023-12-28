c9d8609c0ff8e8f1ab72dc2d6307429514687b7e
Anurag Dhote <anuragdhote392@gmail.com>
2023-12-28 19:19:26 +0530

    feat - add dark theme and navbar complete

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Input } from './ui/input'
-import Link from 'next/link'
 
 type Props = {}
 
-const Navbar = (props: Props) => {
+const Logo = (props: Props) => {
     return (
         <nav className="relative flex items-center justify-between p-4 lg:px-6">
             <div className="block flex-none md:hidden">
@@ -14,33 +12,24 @@ const Navbar = (props: Props) => {
             </div>
             <div className="flex w-full items-center">
                 <div className="flex w-full md:w-1/3">
-                    <Link className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6" href="/">
+                    <a className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6" href="/">
                         <div className="flex flex-none items-center justify-center border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-black h-[40px] w-[40px] rounded-xl">
-                            <svg xmlns="http://www.w3.org/2000/svg" aria-label="Swift Kart logo" viewBox="0 0 32 28" className="h-4 w-4 fill-black dark:fill-white"><path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z"></path><path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" aria-label="Acme Store logo" viewBox="0 0 32 28" className="h-4 w-4 fill-black dark:fill-white"><path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z"></path><path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z"></path></svg>
                         </div>
-                        <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block dark:text-white">Swift Kart</div>
-                    </Link>
+                        <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block dark:text-white">Acme Store</div>
+                    </a>
                     <ul className="hidden gap-6 text-sm md:flex md:items-center">
-                        <li>
-                            <Link className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300" href="/search">
-                                All
-                            </Link>
+                        <li><a className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300" href="/search">All</a>
                         </li>
-                        <li>
-                            <Link className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300" href="/search/shirts">
-                                Shirts
-                            </Link>
+                        <li><a className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300" href="/search/shirts">Shirts</a>
                         </li>
-                        <li>
-                            <Link className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300" href="/search/stickers">
-                                Stickers
-                            </Link>
+                        <li><a className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300" href="/search/stickers">Stickers</a>
                         </li>
                     </ul>
                 </div>
                 <div className="hidden justify-center md:flex md:w-1/3">
                     <form className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
-                        <Input type="text" placeholder="Search for products..." className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400" name="search" value=""/>
+                        <input type="text" placeholder="Search for products..." className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400" name="search" value=""/>
                         <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" className="h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
                             </svg>
@@ -60,4 +49,5 @@ const Navbar = (props: Props) => {
     )
 }
 
-export default Navbar
+export default Logo
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,7 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        blackBg: "#171717" 
       },
       borderRadius: {
         lg: "var(--radius)",
