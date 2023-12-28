6ecf531a8c9f945546b13d5bf5ed7f17aab8c2de
Anurag Dhote <anuragdhote392@gmail.com>
2023-12-28 17:47:09 +0530

    added input component from shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from 'geist/font/sans'
 import './globals.css'
 import ReduxProvider from '@/redux/provider'
+import Navbar from '@/components/Navbar'
 
 const defaultUrl = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
@@ -22,6 +23,7 @@ export default function RootLayout({
 			<body className="bg-background text-foreground">
 				<main className="min-h-screen flex flex-col items-center">
 					<ReduxProvider>
+						<Navbar />
 						{children}
 					</ReduxProvider>
 				</main>
