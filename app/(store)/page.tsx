62480e40a0216ac681966064ace8c2b2aac3dece
Anurag Dhote <anuragdhote392@gmail.com>
2024-01-05 16:29:15 +0530

    feat - initial carousel setup in home page

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,28 +1,11 @@
-"use client"
 
-import Link from "next/link";
-import { useAppSelector } from "@/redux/store";
-import { ToastAction } from "@/components/ui/toast";
-import { useToast } from "@/components/ui/use-toast";
-import { Button } from "@/components/ui/button";
+import HomeCarousel from "@/components/HomeCarousel"
 
 export default function Index() {
-	const isAuthenticated = useAppSelector((state) => state.authReducer.value.isAuthenticated);
-	const { toast } = useToast();
-
 	return (
 		<>
-			<div>Ecommerce App</div>
-			{/* <Button
-				variant="outline"
-				onClick={() => {
-					toast({
-						description: "Your message has been sent.",
-					})
-				}}
-			>
-				Show Toast
-			</Button> */}
+			{/* <div>Ecommerce App</div> */}
+			<HomeCarousel />
 		</>
 	)
 }
