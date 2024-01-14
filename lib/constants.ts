cb7069235abbf9923b97448f7341a285ba8c2798
Anurag Dhote <anuragdhote392@gmail.com>
2024-01-14 18:18:51 +0530

    fix - fixing SITE_URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 
 export const SEARCH_PRODUCTS_PER_PAGE = 12;
 
-export const SITE_URL = process.env.NODE_ENV === "production" ? `${process.env.NEXT_PUBLIC_VERCEL_URL}` : "http://localhost:3000"
+export const SITE_URL = process.env.NODE_ENV === "production" ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : "http://localhost:3000"
