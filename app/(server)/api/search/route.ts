affaf733fd422b11758cd7486847c567073521cb
Anurag Dhote <anuragdhote392@gmail.com>
2024-01-23 20:29:21 +0530

    fix - fetched products on server components itself

--- app/(server)/api/search/route.ts
+++ app/(server)/api/search/route.ts
@@ -1,13 +1,8 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (request: NextRequest) => {
+const getProductsByCategory = async (take: number, skip: number, categoryName: string | null) => {
     try {
-        const params = request.nextUrl.searchParams;
-        const take = params.get("take") || 12;
-        const skip = params.get("skip") || 0;
-        const categoryName = params.get("category");
-
         let products, totalProducts;
         if (categoryName) {
             const category = await prisma.categories.findFirst({where:{name: categoryName}});
@@ -26,8 +21,47 @@ export const GET = async (request: NextRequest) => {
                 prisma.products.count({})
             ])
         }
-        return NextResponse.json({ error: undefined, response: products, totalProducts });
+        return { error: undefined, response: products, totalProducts }; 
     } catch (error) {
-        return NextResponse.json({ error, response: undefined });
+        return { error, response: undefined, totalProducts: undefined };
     }
 }
+
+export const GET = async (request: NextRequest) => {
+    const params = request.nextUrl.searchParams;
+    const take = Number(params.get("take")) || 12;
+    const skip = Number(params.get("skip")) || 0;
+    const categoryName = params.get("category");
+    
+    const { error, response, totalProducts } = await getProductsByCategory(take, skip, categoryName);
+
+    if (error) {
+        return NextResponse.json({error, response: null});
+    }
+    return NextResponse.json({ error: undefined, response, totalProducts });
+}
+    
+    
+    // try {
+    //     let products, totalProducts;
+    //     if (categoryName) {
+    //         const category = await prisma.categories.findFirst({where:{name: categoryName}});
+    //         if (!category) {
+    //             throw new Error("Category not available!");
+    //         }
+    //         const {id: category_id} = category;
+    // 
+    //         [products, totalProducts] = await Promise.all([
+    //             prisma.products.findMany({ where: { category_id }, take: Number(take), skip: Number(skip)}),
+    //             prisma.products.count({ where: { category_id }})
+    //         ])
+    //     } else {
+    //         [products, totalProducts] = await Promise.all([
+    //             prisma.products.findMany({take: Number(take), skip: Number(skip)}),
+    //             prisma.products.count({})
+    //         ])
+    //     }
+    //     return NextResponse.json({ error: undefined, response: products, totalProducts });
+    // } catch (error) {
+    //     return NextResponse.json({ error, response: undefined });
+    // }
--- app/(store)/search/[category]/page.tsx
+++ app/(store)/search/[category]/page.tsx
@@ -1,17 +1,34 @@
 import React, { Suspense } from 'react'
+import prisma from "@/lib/db";
 import type { products as ProductType } from '@prisma/client';
 
 import { SEARCH_PRODUCTS_PER_PAGE, SITE_URL } from '@/lib/constants';
 import SearchCard from '@/components/search/SearchCard';
 import SearchCardLoading from '@/components/search/SearchCardLoading';
 
-async function getCategoryProducts(categoryName: string) {
+const getProductsByCategory = async (categoryName: string | null) => {
     try {
-        const res = await fetch(`${SITE_URL}/api/search?category=${categoryName}`);
-        const { error, response, totalProducts } = await res.json();
-        return { error, products: response, totalProducts };
+        let products, totalProducts;
+        if (categoryName) {
+            const category = await prisma.categories.findFirst({where:{name: categoryName}});
+            if (!category) {
+                throw new Error("Category not available!");
+            }
+            const {id: category_id} = category;
+
+            [products, totalProducts] = await Promise.all([
+                prisma.products.findMany({ where: { category_id }}),
+                prisma.products.count({ where: { category_id }})
+            ])
+        } else {
+            [products, totalProducts] = await Promise.all([
+                prisma.products.findMany({}),
+                prisma.products.count({})
+            ])
+        }
+        return { error: undefined, response: products, totalProducts }; 
     } catch (error) {
-        return { error };
+        return { error, response: undefined, totalProducts: undefined };
     }
 }
 
@@ -21,7 +38,7 @@ type Props = {
 
 const Category = async ({ params }: Props) => {
     const categoryName = params.category;
-    const { error, products, totalProducts } = await getCategoryProducts(categoryName);
+    const { error, response: products, totalProducts } = await getProductsByCategory(categoryName);
 
     if (error) {
         console.log(error);
@@ -39,8 +56,9 @@ const Category = async ({ params }: Props) => {
                     ))}
                 </div>}
             >
-                <div className='grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                    {products.map((product: ProductType) => (
+                <p className="my-2">Showing {totalProducts} search results</p>
+                <div className='grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-4 md:mb-8 lg:mb-12'>
+                    {products && products.map((product: ProductType) => (
                         <SearchCard key={product.id} product={product} />
                     ))}
                 </div>
